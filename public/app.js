a2e718a6a57335d582396f563a187adb201a80a1
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-11-15 20:55:56 -0300

    table creation

--- public/app.js
+++ public/app.js
@@ -1,21 +1,34 @@
 // Make a request for a user with a given ID
 
-getData = () => {
-  axios
-    .get("http://localhost:3000/api/dolar")
-    .then(function (response) {
-      // handle success
-      return response.data;
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+let dolar;
+
+const data = async () => {
+  try {
+    dolar = await axios.get("http://localhost:3000/api/dolar");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const tablaDolar = document.getElementById("tabla-dolar");
+data();
 
-let data = getData();
+setTimeout(() => {
+  console.log("dolar", dolar.data);
+  let obj = Object.keys(dolar.data);
+  Object.keys(dolar.data).forEach((key) => {
+    const hilera = document.createElement("tr");
+
+    let tipoDolar = document.createElement("td");
+    let compraDolar = document.createElement("td");
+    let ventaDolar = document.createElement("td");
+    tipoDolar.innerHTML = key;
+    compraDolar.innerHTML = dolar.data[key].compra;
+    ventaDolar.innerHTML = dolar.data[key].venta;
+    hilera.appendChild(tipoDolar);
+    hilera.appendChild(compraDolar);
+    hilera.appendChild(ventaDolar);
+
+    tablaDolar.appendChild(hilera);
+  });
+}, 3000);
