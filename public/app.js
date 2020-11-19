142e3d4fc61f538d20f73396ae99cb29f1eb6638
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-11-19 00:21:42 -0300

    style to html

--- public/app.js
+++ public/app.js
@@ -5,6 +5,9 @@ let dolar;
 const data = async () => {
   try {
     dolar = await axios.get("https://api-scrapper.herokuapp.com/api/dolar");
+
+    // return API
+    // {"dolarbanconacion":{"compra":79.25,"venta":85.25},"dolarblue":{"compra":159,"venta":164},"dolarsolidario":{"venta":140.66},"dolarmayorista":{"compra":79.95,"venta":80.15},"dolarmepcontado":{"compra":146.05,"venta":147.01},"dolarcdocliqui":{"compra":149.07,"venta":149.14}}
   } catch (error) {
     console.log(error);
   }
@@ -12,6 +15,40 @@ const data = async () => {
 
 const tablaDolar = document.getElementById("tabla-dolar");
 data();
+
+function cambioTipoDolar(tipoDolar) {
+  switch (tipoDolar) {
+    case "dolarbanconacion":
+      return "Dólar Banco Nación";
+      break;
+    case "dolarblue":
+      return "Dólar Blue";
+      break;
+    case "dolarsolidario":
+      return "Dólar Solidario";
+      break;
+    case "dolarmayorista":
+      return "Dólar Mayorista";
+      break;
+    case "dolarmepcontado":
+      return "Dólar MEP Contado";
+      break;
+    case "dolarcdocliqui":
+      return "Dólar Contado c/liqui";
+      break;
+    default:
+      return tipoDolar;
+      break;
+  }
+}
+
+function reval(value) {
+  if (value === undefined) {
+    return "-";
+  } else {
+    return `$${value}`;
+  }
+}
 
 setTimeout(() => {
   console.log("dolar", dolar.data);
@@ -22,9 +59,13 @@ setTimeout(() => {
     let tipoDolar = document.createElement("td");
     let compraDolar = document.createElement("td");
     let ventaDolar = document.createElement("td");
-    tipoDolar.innerHTML = key;
-    compraDolar.innerHTML = dolar.data[key].compra;
-    ventaDolar.innerHTML = dolar.data[key].venta;
+    tipoDolar.className = "row";
+    compraDolar.className = "row";
+    ventaDolar.className = "row";
+
+    tipoDolar.innerHTML = cambioTipoDolar(key);
+    compraDolar.innerHTML = reval(dolar.data[key].compra);
+    ventaDolar.innerHTML = reval(dolar.data[key].venta);
     hilera.appendChild(tipoDolar);
     hilera.appendChild(compraDolar);
     hilera.appendChild(ventaDolar);
