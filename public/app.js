23876fffdb7aa737ddae5dedc06b6e1e8845fe84
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-11-20 13:47:42 -0300

    change on time to load API

--- public/app.js
+++ public/app.js
@@ -6,6 +6,26 @@ const data = async () => {
   try {
     dolar = await axios.get("https://api-scrapper.herokuapp.com/api/dolar");
 
+    let obj = Object.keys(dolar.data);
+    Object.keys(dolar.data).forEach((key) => {
+      const hilera = document.createElement("tr");
+
+      let tipoDolar = document.createElement("td");
+      let compraDolar = document.createElement("td");
+      let ventaDolar = document.createElement("td");
+      tipoDolar.className = "row";
+      compraDolar.className = "row";
+      ventaDolar.className = "row";
+
+      tipoDolar.innerHTML = cambioTipoDolar(key);
+      compraDolar.innerHTML = reval(dolar.data[key].compra);
+      ventaDolar.innerHTML = reval(dolar.data[key].venta);
+      hilera.appendChild(tipoDolar);
+      hilera.appendChild(compraDolar);
+      hilera.appendChild(ventaDolar);
+
+      tablaDolar.appendChild(hilera);
+    });
     // return API
     // {"dolarbanconacion":{"compra":79.25,"venta":85.25},"dolarblue":{"compra":159,"venta":164},"dolarsolidario":{"venta":140.66},"dolarmayorista":{"compra":79.95,"venta":80.15},"dolarmepcontado":{"compra":146.05,"venta":147.01},"dolarcdocliqui":{"compra":149.07,"venta":149.14}}
   } catch (error) {
@@ -49,30 +69,6 @@ function reval(value) {
     return `$${value}`;
   }
 }
-
-setTimeout(() => {
-  console.log("dolar", dolar.data);
-  let obj = Object.keys(dolar.data);
-  Object.keys(dolar.data).forEach((key) => {
-    const hilera = document.createElement("tr");
-
-    let tipoDolar = document.createElement("td");
-    let compraDolar = document.createElement("td");
-    let ventaDolar = document.createElement("td");
-    tipoDolar.className = "row";
-    compraDolar.className = "row";
-    ventaDolar.className = "row";
-
-    tipoDolar.innerHTML = cambioTipoDolar(key);
-    compraDolar.innerHTML = reval(dolar.data[key].compra);
-    ventaDolar.innerHTML = reval(dolar.data[key].venta);
-    hilera.appendChild(tipoDolar);
-    hilera.appendChild(compraDolar);
-    hilera.appendChild(ventaDolar);
-
-    tablaDolar.appendChild(hilera);
-  });
-}, 3000);
 
 //https://www.lacapital.com.ar/rss/economia.xml
 //https://www.clarin.com/rss.html
